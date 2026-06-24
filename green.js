5344f906ce254c1e40cbd101e28a21b2db9569eb
akib413 <hasanakibul216@gmail.com>
2026-06-24 23:32:22 +0600

    add api and fetch

--- green.js
+++ green.js
@@ -16,3 +16,39 @@ async function categoriesList() {
     }
 }
 categoriesList()
+
+let chooseTrees = document.getElementById('trees-container')
+async function allPlants() {
+    let res = await fetch("https://openapi.programming-hero.com/api/plants")
+    let data = await res.json()
+    console.log(data)
+
+    let findPlants = data.plants
+    console.log(findPlants)
+
+    findPlants.forEach(plant => {
+        let div = document.createElement('div')
+        div.className = 'card bg-base-100 shadow-sm'
+        div.innerHTML = `
+        
+                    <figure class="px-10 pt-10">
+                        <img src=${plant.image}
+                            alt="Shoes" class="rounded-xl" />
+                    </figure>
+                    <div class="card-body">
+                        <h2 class="card-title">${plant.name}</h2>
+                        <p>${plant.description}</p>
+                        <div class="tree-price w-full flex justify-between">
+                            <h3>Fruit Tree</h3>
+                            <h3>500</h3>
+                        </div>
+                        <div class="card-actions w-full">
+                            <button class="btn btn-primary w-full">Buy Now</button>
+                        </div>
+                    </div>
+                
+        `
+        chooseTrees.appendChild(div)
+    });
+}
+allPlants()
