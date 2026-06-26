4710b5983b638a8d25a9ce0db412101aa4272903
akib413 <hasanakibul216@gmail.com>
2026-06-26 23:51:54 +0600

    add dynamic category

--- green.js
+++ green.js
@@ -7,11 +7,21 @@ async function categoriesList() {
     let showCategory = data.categories
     console.log(showCategory)
 
-    for(let category of showCategory){
+    for (let category of showCategory) {
         console.log(category.category_name)
         let btn = document.createElement('button')
-        btn.className = 'btn btn-soft btn-primary w-full'
+        btn.className = 'category-btn btn btn-soft btn-success w-full'
         btn.innerHTML = category.category_name
+        btn.onclick = () => {
+            selectedCategory(category.id)
+
+            let buttons = document.querySelectorAll('.category-btn')
+
+            buttons.forEach(button => {
+                button.classList.remove("btn-active");
+            });
+            btn.classList.add("btn-active");
+        }
         allCategories.appendChild(btn)
     }
 }
@@ -52,3 +62,40 @@ async function allPlants() {
     });
 }
 allPlants()
+
+async function selectedCategory(categoryId) {
+    console.log(categoryId)
+    let res = await fetch(`https://openapi.programming-hero.com/api/category/${categoryId}`)
+    let data = await res.json()
+    console.log(data.plants)
+
+    let findPlants = data.plants
+    console.log(findPlants)
+
+    chooseTrees.innerHTML = ""
+
+    findPlants.forEach(plant => {
+        let div = document.createElement('div')
+        div.className = 'card bg-base-100 shadow-sm'
+        div.innerHTML = `
+        
+                    <figure class="px-10 pt-10">
+                        <img src=${plant.image}
+                            alt="Shoes" class="rounded-xl" />
+                    </figure>
+                    <div class="card-body">
+                        <h2 class="card-title">${plant.name}</h2>
+                        <p>${plant.description}</p>
+                        <div class="tree-price w-full flex justify-between">
+                            <h3>Fruit Tree</h3>
+                            <h3>500</h3>
+                        </div>
+                        <div class="card-actions w-full">
+                            <button class="btn btn-primary w-full">Buy Now</button>
+                        </div>
+                    </div>
+                
+        `
+        chooseTrees.appendChild(div)
+    });
+}
